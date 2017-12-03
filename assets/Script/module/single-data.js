c4718473f11bfb4bf6e65bf8238e2738dd1e7e8a
dragon <dragon8897@163.com>
2017-12-03 23:47:07 +0800

    add feature: single logic fake data

---
+++ assets/Script/module/single-data.js
@@ -0,0 +1,24 @@
+module.exports = {
+    Players : [
+        {
+            name: 'flash',
+            coin: 2000,
+        },
+        {
+            name: 'robot2',
+            coin: 3000,
+        },
+        {
+            name: 'bat man',
+            coin: 99999999999
+        },
+        {
+            name: 'super man',
+            coin: 60000,
+        },
+        {
+            name: 'wonder woman',
+            coin: 8000,
+        }
+    ],
+}
--- assets/Script/module/single-logic.js
+++ assets/Script/module/single-logic.js
@@ -1,10 +1,20 @@
 const State = require('state.com');
+const Pokers = require('pokers');
+const Utils = require('utils');
+
+const PLAYER_MAX_COUNT = 3;
 
 class SingleLogic {
     constructor(delegate) {
         this.delegate = delegate;
         this.model = null;
         this.instance = null;
+        this.arrPlayers = [];
+        this.arrPokers = [];
+        // 决定地主的牌
+        this.nLorderIndex = 0;
+        // 轮数
+        this.nRound = 0;
         this._initState();
     }
 
@@ -16,17 +26,37 @@ class SingleLogic {
 
         let idle = new State.State('等待', this.model);
         let deal = new State.State('发牌', this.model);
-        let elect = new State.State('选举地主', this.model);
+        let elect = new State.State('叫地主', this.model);
+        let grab = new State.State('抢地主', this.model);
         let raise = new State.State('加倍', this.model);
         let play = new State.State('操作', this.model);
 
         initial.to(idle);
 
         idle.entry( () => {
-            this.delegate.onStateChange(SingleLogic.StateType.Idle);
+            this.arrPlayers = [];
         });
 
         deal.entry ( () => {
+            this.arrPokers = Pokers.randomPokers();
+            let nLorderIndex = Utils.getRandomInt(0, this.arrPokers.length - Pokers.KeyCount);
+            this.nLorderIndex = Math.floor(nLorderIndex / PLAYER_MAX_COUNT);
+            let nEachCount = (Pokers.Count - Pokers.KeyCount) / PLAYER_MAX_COUNT;
+            let nLorderPos = nLorderIndex % nEachCount * PLAYER_MAX_COUNT + this.nLorderIndex;
+            this.delegate.onGetDealPokers({
+                handPokers : this.arrPokers.slice(0, nEachCount),
+                lorderPoker : {
+                    poker: this.arrPokers[nLorderIndex],
+                    index: nLorderPos,
+                }
+            });
+            setTimeout(() => {
+                this._evaluate(SingleLogic.StateEvent.Next);
+            }, 2000);
+        });
+
+        elect.entry( () => {
+            this.delegate.onElectLorder(this.nLorderIndex);
         });
 
         // create a State machine instance
@@ -45,6 +75,21 @@ class SingleLogic {
         this.evaluating = true;
         State.evaluate(this.model, this.instance, msg);
         this.evaluating = false;
+    }
+
+    addPlayer(player) {
+        this.arrPlayers.push(player);
+        if (this.arrPlayers.length === PLAYER_MAX_COUNT) {
+            this._evaluate(SingleLogic.StateEvent.Next);
+        }
+    }
+
+    electLorder(nPlayerIndex) {
+
+    }
+
+    giveupLorder(nPlayerIndex) {
+
     }
 }
 
@@ -77,6 +122,8 @@ SingleLogic.StateEvent = {
     PlayNext: 'play_next',
     PlayPrev: 'play_prev',
     PlayPass: 'play_pass',
+    Next : 'next',
+    Turn : 'turn',
 };
 
 module.exports = SingleLogic;
